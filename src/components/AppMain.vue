6d02ccc1d7296ad068f1a17f5c7aae7996e7a8e4
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-04-17 18:42:00 +0200

    Job almost completed

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -18,7 +18,7 @@ export default {
 
 <template>
     <main>
-        <div class="grid">
+        <div class="grid ms_grid">
             <div class="row">
                 <div class="col-3 py-3" v-for="card in store.cards" :key="card.id">
                     <AppCards :card="card"/>
@@ -31,6 +31,15 @@ export default {
 <style scoped lang="scss">
 @use "../style/general" as *;
 @use "../style/mixins" as *;
+@use "../style/variables" as *;
 
-// @use "../style/variables" as *;
+main {
+    background-color: $main-background-color;
+.ms_grid {
+    background-color: white;
+    width: 80%;
+    margin: 0 auto;
+    padding: 10px 30px;
+}
+}
 </style>
