dc35a6a1d616b0a1ac1b9d073dd1ca5e731f988c
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-04-17 16:07:42 +0200

    Project Scaffolding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<!-- import axios from "axios"; -->
+<script>
+export default {
+    name: "AppHeader",
+    components: {
+
+    },
+
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template></template>
+
+<style scoped lang="scss">
+@use "../style/general" as *;
+// @use "../style/mixins" as *;
+// @use "../style/variables" as *;
+</style>
